fec9c5bac0ca616d7a9beb77edb7fb0d1897fcc8
fsoc <root@fsoc.space>
2021-04-24 22:30:50 +0200

    Clean up old files, finish new index

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,20 @@
-const App = ({ Component, pageProps }) => (<>
-    <main className={'flex-grow'}>
+const Footer = () => (<>
+    <footer className={'flex flex-row gap-2 ty container text-sm py-2 border-t border-gray-300'}>
+        <span>Built with <a href={'https://github.com/fs-c/dhow/tree/rewrite'}>fs-c/dhow</a>.</span>
+
+        <div className={'flex-grow'} />
+
+        <a href={'/imprint'}>/imprint</a>
+        <a href={'/privacy'}>/privacy</a>
+    </footer>
+</>);
+
+const App = ({ Component, pageProps = {} }) => (<>
+    <main className={`flex-grow ${pageProps.wrapperClassName || ''}`}>
         <Component {...pageProps} />
     </main>
+
+    <Footer />
 </>);
 
 export default App;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,30 @@
 const Home = () => {
     return (<>
-        <div className={'h-screen w-full flex flex-col justify-center items-center px-4'}>
-            <div className={'flex flex-col items-center ty'}>
-                <img src={'/assets/logo.svg'} alt={'fsoc'} />
+        <div className={'flex flex-col items-center prose container'}>
+            <img src={'/assets/logo.svg'} alt={'fsoc'} />
 
-                <nav className={'w-full flex flex-row justify-between ty'}>
-                    <a href={'/about'}>/about</a> <a href={'/projects'}>/projects</a> <a href={'/words'}>/words</a> <a href={'/files'}>/files</a>
-                </nav>
+            <nav className={'w-full flex flex-row justify-between mb-2'}>
+                <a href={'/about'}>/about</a> <a href={'/projects'}>/projects</a> <a href={'/words'}>/words</a> <a href={'/files'}>/files</a>
+            </nav>
 
-                <p className={'mt-2'}>
-                    Hey there, I'm Laurenz Weixlbaumer, although I usually go 
-                    by <code>fsoc</code> online. I'm a software developer living in 
-                    Austria and currently a freelancer.
-                </p>
-            </div>
+            <p>
+                Hey there, I'm Laurenz Weixlbaumer, although I usually go 
+                by <code>fsoc</code> online. I'm a software developer living in 
+                Austria and currently a freelancer.
+            </p>
+
+            <p>
+                You can contact me by email via <code>root@ this domain</code>. I'm on <a href={'https://github.com/fs-c/'}>github/fs-c</a> and <a href={'https://gitlab.com/fsoc'}>gitlab/fsoc</a>. 
+                Sometimes I play games on <a href={'http://steamcommunity.com/profiles/76561198091491690'}>steam/f50c</a>.
+            </p>
         </div>
     </>);
 };
 
 export default Home;
+
+export const getProps = () => {
+    return {
+        wrapperClassName: 'w-full flex flex-col justify-center items-center',
+    };
+}
